be3e07a8b635c07fe994e8373f258ca737b2dc3c
Mike North <michael.l.north@gmail.com>
2016-07-30 23:27:56 +0800

    Blueprint: Chrome in Travis-CI

---
+++ blueprints/travis-chrome/index.js
@@ -0,0 +1,17 @@
+/*jshint node:true*/
+module.exports = {
+  description: 'Setup Travis-CI config for running tests against chrome',
+  normalizeEntityName: function() {
+
+  }
+  // locals: function(options) {
+  //   // Return custom template variables here.
+  //   return {
+  //     foo: options.entity.options.foo
+  //   };
+  // }
+
+  // afterInstall: function(options) {
+  //   // Perform extra work here.
+  // }
+};
