70a182e0f496f63c08609ff6c3bb12f0368bd157
Mike North <michael.l.north@gmail.com>
2016-07-31 00:49:28 +0800

    Circle-ci blueprints

---
+++ blueprints/circle-ci-chrome/index.js
@@ -0,0 +1,5 @@
+/*jshint node:true*/
+module.exports = {
+  description: 'Setup Circle-CI config for running tests against chrome',
+  normalizeEntityName: function() { }
+};
---
+++ blueprints/circle-ci-init/index.js
@@ -0,0 +1,5 @@
+/*jshint node:true*/
+module.exports = {
+  description: 'Setup Circle-CI config for running tests against phantomjs',
+  normalizeEntityName: function() { }
+};
